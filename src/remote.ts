22ac76d62bc1cad1858ae3fbfe745facea5bb1cd
liutao <vaceliu@gmail.com>
2025-04-10 16:31:36 +0800

    增加远程调用脚本

---
+++ src/remote.ts
@@ -0,0 +1,34 @@
+import express, { Request, Response } from "express";
+import { createServer } from "./server.js";
+import { SSEServerTransport } from "@modelcontextprotocol/sdk/server/sse.js";
+
+const server = createServer();
+
+// ... set up server resources, tools, and prompts ...
+
+const app = express();
+
+// to support multiple simultaneous connections we have a lookup object from
+// sessionId to transport
+const transports: { [sessionId: string]: SSEServerTransport } = {};
+
+app.get("/sse", async (_: Request, res: Response) => {
+  const transport = new SSEServerTransport('/messages', res);
+  transports[transport.sessionId] = transport;
+  res.on("close", () => {
+    delete transports[transport.sessionId];
+  });
+  await server.connect(transport);
+});
+
+app.post("/messages", async (req: Request, res: Response) => {
+  const sessionId = req.query.sessionId as string;
+  const transport = transports[sessionId];
+  if (transport) {
+    await transport.handlePostMessage(req, res);
+  } else {
+    res.status(400).send('No transport found for sessionId');
+  }
+});
+
+app.listen(3001);
